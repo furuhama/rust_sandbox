732d2bcfb21167f8e91640d27f8e2095da75f28f
furuhama <furuhama@users.noreply.github.com>
2018-01-10 00:15:59 +0900

    Edit main.rs to get std input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
     println!("hello, rust!");
+
+    println!("please input some words");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("failed to read line");
+
+    println!("Input: {}", guess);
 }
