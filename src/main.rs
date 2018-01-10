3921a998c983f16d93cbd9160e86cc2c5eeaa264
furuhama <furuhama@users.noreply.github.com>
2018-01-10 11:23:37 +0900

    Edit main.rs to use let var

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::io;
+// use std::io;
 
 fn main() {
-    println!("hello, rust!");
+    println!("====hello, rust!====");
 
-    println!("please input some words");
+    // println!("please input some words");
+    // let mut guess = String::new();
+    // io::stdin().read_line(&mut guess).expect("failed to read line");
+    // println!("Input: {}", guess);
 
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("failed to read line");
-
-    println!("Input: {}", guess);
+    let x: i32 = 2836;
+    let y: i32;
+    println!("x: {}, y: {}", x, y);
 }
