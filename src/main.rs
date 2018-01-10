534e4cf3d155988ce3f465256b5ed17647484976
furuhama <furuhama@users.noreply.github.com>
2018-01-10 21:03:18 +0900

    Add hello_rust to assert ASCII ART

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ extern crate hello_world;
 use hello_world::my_module;
 
 fn main() {
-    println!("==== hello, rust! ====\n");
+    println!("{}", my_module::hello_rust());
 
     // println!("please input some words");
     // let mut guess = String::new();
     // io::stdin().read_line(&mut guess).expect("failed to read line");
     // println!("Input: {}", guess);
 
-    println!("{}", my_module::fizzbuzz::fizzbuzz(35));
+    println!("{}", my_module::fizzbuzz(35));
 
-    println!("{}", my_module::fibonacci::get_fibonacci(90));
+    println!("{}", my_module::get_fibonacci(90));
 }
 
--- src/my_module/mod.rs
+++ src/my_module/mod.rs
@@ -1,5 +1,10 @@
 // set modules in my_module
 
-pub mod fibonacci;
-pub mod fizzbuzz;
+pub use self::fibonacci::get_fibonacci;
+pub use self::fizzbuzz::fizzbuzz;
+pub use self::hello_rust::hello_rust;
+
+mod fibonacci;
+mod fizzbuzz;
+mod hello_rust;
 
