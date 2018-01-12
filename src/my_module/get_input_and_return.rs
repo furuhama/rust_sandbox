3cae98744bce49f715d28599926a30beb2b8f372
furuhama <furuhama@users.noreply.github.com>
2018-01-12 23:55:40 +0900

    Add comment

--- src/my_module/get_input_and_return.rs
+++ src/my_module/get_input_and_return.rs
@@ -3,7 +3,9 @@ use std::io;
 pub fn get_and_print_input() {
     println!("please input some words");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed to read line.");
+    io::stdin().read_line(&mut guess) // io::stdin().read_line(hoge) は io::Result インスタンス
+        .expect("failed to read line.");
+        // expect は io::Result が err のときにその命令を停止して、引数の文字列を標準出力から返す
     println!("your input: {}", guess);
 }
 
