ab5b06b4700ec2bcd87b9cd0a00160bad4010090
furuhama <furuhama@users.noreply.github.com>
2018-01-19 00:29:48 +0900

    Prictice normal vars & consts

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,20 @@ use hello_world::my_module;
 fn main() {
     my_module::hello_rust();
 
-    my_module::guess_the_number();
+    // my_module::guess_the_number();
 
     // my_module::fizzbuzz(35);
 
-    let n: i32 = 90;
-    println!("{}th fibonacci number: {}", n, my_module::get_fibonacci(n));
+    // let n: i32 = 90;
+    // println!("{}th fibonacci number: {}", n, my_module::get_fibonacci(n));
+
+    let mut x = 5;
+    println!("{}", x);
+    x = 10;
+    println!("{}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+
+    println!("{}", MAX_POINTS);
 }
 
