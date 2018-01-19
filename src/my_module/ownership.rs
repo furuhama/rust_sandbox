be5d265138ca5fb5b5865a93b5fed3a04d3ad1c6
furuhama <furuhama@users.noreply.github.com>
2018-01-20 01:11:09 +0900

    Add more some functions for checking ownership

--- src/my_module/ownership.rs
+++ src/my_module/ownership.rs
@@ -6,11 +6,22 @@ pub fn practice_ownership() {
 
     s.push_str(", rust!");
 
+    // String move its ownership
     takes_ownership(s);
 
     let x = 5;
 
+    // integer makes its copy automatically
+    let x = 5;
     makes_copy(x);
+
+    let s2 = gives_ownership();
+
+    let s3 = take_and_give_back(s2);
+
+    let (s4, len) = return_string_and_length(s3);
+
+    println!("The length of '{}' is {}", s4, len);
 }
 
 fn takes_ownership(some_string: String) {
@@ -19,4 +30,20 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_int: i32) {
     println!("{}", some_int);
+}
+
+fn gives_ownership() -> String {
+    let s = String::from("hoge piyo");
+    // if function returns a value, it move its ownership
+    s
+}
+
+fn take_and_give_back(a_string: String) -> String {
+    a_string
+}
+
+fn return_string_and_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
