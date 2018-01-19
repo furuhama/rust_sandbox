07695203601b3dbe304309673c36a351cb799ba5
furuhama <furuhama@users.noreply.github.com>
2018-01-19 12:01:39 +0900

    Add comment & Type practice

--- src/my_module/basic_concepts.rs
+++ src/my_module/basic_concepts.rs
@@ -1,6 +1,8 @@
 // practice Rust basic concepts
 
 pub fn run_basic_concepts() {
+    // Vatiables and Mutability
+
     // to reassign something for one variable
     // use `mut`
     let mut x = 10;
@@ -18,5 +20,21 @@ pub fn run_basic_concepts() {
     let piyo = piyo + 10;
     let piyo = piyo * 3;
     println!("{}", piyo);
+
+
+    // Types
+
+    // type annotation
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("{}", guess);
+
+    // character type
+    let emoji = '😻';
+    println!("cutest emoji: {}", emoji);
+
+    // Grouping values into tuples
+    let tup: (i32, i32, i32) = (10, 50, 64);
+    let (tup_1, tup_2, tup_3) = tup;
+    println!("tuple values: {}, {}, {}", tup_1, tup_2, tup_3);
 }
 
