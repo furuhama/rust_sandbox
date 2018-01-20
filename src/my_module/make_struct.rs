ccbdc273233753e324646cf0da5e7d4d345d7852
furuhama <furuhama@users.noreply.github.com>
2018-01-20 20:17:33 +0900

    Add build_user, Color struct by tuple

--- src/my_module/make_struct.rs
+++ src/my_module/make_struct.rs
@@ -17,6 +17,27 @@ pub fn make_struct() {
     // redefine email
     u1.email = String::from("fuga@example.com");
     println!("{}", u1.email);
+
+    let u2 = build_user(
+        String::from("mofumofu@example.com"),
+        String::from("furufuru"),
+    );
+    println!("email: {}, name: {}", u2.email, u2.name);
+
+    // update instance
+    let u3 = User {
+        email: String::from("neko@example.com"),
+        name: String::from("nyanko"),
+        ..u2
+    };
+    println!(
+        "name: {}\nemail: {}\ncount: {}\nactive: {}",
+        u3.name, u3.email, u3.sign_in_count, u3.active
+    );
+
+    // make instance by tuple struct
+    let color_test = Color(0, 10, 352);
+    println!("{}, {}, {}", color_test.0, color_test.1, color_test.2);
 }
 
 struct User {
@@ -25,3 +46,15 @@ struct User {
     sign_in_count: u64,
     active: bool,
 }
+
+fn build_user(email: String, name: String) -> User {
+    User {
+        email,
+        name,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+// just make struct by tuple
+struct Color(i32, i32, i32);
