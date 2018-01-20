efe02c8357131cb5702a81a43e5454f4ac59d4ac
furuhama <furuhama@users.noreply.github.com>
2018-01-20 18:44:43 +0900

    Add functions using slice

--- src/my_module/slice.rs
+++ src/my_module/slice.rs
@@ -1,5 +1,70 @@
 // Slices
 
 pub fn slice() {
-    println!("hoge");
+    let s = String::from("hoge fuga piyo");
+    let num = first_word_index(&s);
+
+    println!("{}'s first word ends at {}", s, num);
+
+    // String Slices
+    let s1 = String::from("hello, rust!");
+
+    // String can be accessed by its index
+    let hello = &s1[0..5];
+    let rust = &s1[7..12];
+
+    println!("{}, {}", hello, rust);
+
+    let s2 = String::from("hogehoge f ugaaaaaaa");
+    let s2_first = first_word(&s2);
+    println!("{}", s2_first);
+
+    // use standard String(not String::from) as argument
+    let s3 = "star wars";
+    let s3_first = first_word_str(&s3);
+    println!("{}", s3_first);
+
+    // slice from int array
+    let arr = [1, 2, 3, 4, 5, 6, 7];
+    let arr_slice = &arr[1..4];
+
+    for e in arr_slice {
+        println!("{}", e);
+    }
+}
+
+fn first_word_index(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn first_word_str(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
