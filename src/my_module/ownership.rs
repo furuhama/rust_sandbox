d6af287b55415c9531d9dbcdfcc66d9d0787b20c
furuhama <furuhama@users.noreply.github.com>
2018-01-20 09:49:09 +0900

    Add bollowing & Add comments

--- src/my_module/ownership.rs
+++ src/my_module/ownership.rs
@@ -13,13 +13,24 @@ pub fn practice_ownership() {
     let x = 5;
     makes_copy(x);
 
+    // from inside function, ownership moved
     let s2 = gives_ownership();
 
+    // move ownership from s2 to s3
     let s3 = take_and_give_back(s2);
 
+    // to calculate length
+    // Pattern 1: move String ownership to function once,
+    // and return String & usize as a tuple
     let (s4, len) = return_string_and_length(s3);
 
     println!("The length of '{}' is {}", s4, len);
+
+    // Pattern 2: Use reference (it is called `bollowing` in Rust)
+    let s5 = gives_ownership();
+    let length = use_reference(&s5);
+
+    println!("The length of '{}' is {}", s5, length);
 }
 
 fn takes_ownership(some_string: String) {
@@ -44,4 +55,8 @@ fn return_string_and_length(s: String) -> (String, usize) {
     let length = s.len();
 
     (s, length)
+}
+
+fn use_reference(s: &String) -> usize {
+    s.len()
 }
