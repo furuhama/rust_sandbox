197ef00900c294cbd66700416afd18f35072fc0d
furuhama <furuhama@users.noreply.github.com>
2018-01-21 10:05:03 +0900

    Add IpAddr in some way

--- src/my_module/define_enum.rs
+++ src/my_module/define_enum.rs
@@ -1,14 +1,48 @@
 // use Enum
 
+pub fn define_enum() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    println!("IP v4: {:#?}, IP v6: {:#?}", four, six);
+
+    // use struct
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    // try to Option enum
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+
+    println!("{}, {}, {}", some_number, some_string, absent_number);
+}
+
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
     V6,
 }
 
-pub fn define_enum() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
 
-    println!("IP v4: {:#?}, IP v6: {:#?}", four, six);
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
