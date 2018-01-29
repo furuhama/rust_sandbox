f1af8e37771893c31759d238199c9592f85a7be8
furuhama <furuhama@users.noreply.github.com>
2018-01-29 15:59:14 +0900

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,15 @@ pub mod my_module;
 
 #[cfg(test)]
 mod tests {
+    use my_module;
+
     #[test]
     fn it_works() {
+        my_module::hello_rust();
+    }
+
+    #[test]
+    fn assertion_equal() {
         assert_eq!(2 + 2, 4);
     }
 }
