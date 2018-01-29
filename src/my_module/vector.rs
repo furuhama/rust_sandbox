479950f2a3784c4eb156b59d5e1fab192714b813
furuhama <furuhama@users.noreply.github.com>
2018-01-29 17:17:49 +0900

    Add some example for vector

--- src/my_module/vector.rs
+++ src/my_module/vector.rs
@@ -1,6 +1,46 @@
 // vector
 
 pub fn vector() {
+    // init vector
     let v: Vec<i32> = Vec::new();
     println!("{:?}", v);
+
+    // set value when vector is initialized
+    let v2 = vec![1, 2, 3];
+    println!("{:?}", v2);
+
+    // update vector
+    let mut v3 = Vec::new();
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+    println!("{:?}", v3);
+
+    // update vector (part 2)
+    let mut v4 = vec![0, 1, 2, 3];
+    v4.push(4);
+    v4.push(5);
+    v4.push(6);
+    println!("{:?}", v4);
+
+    // get element from vector
+    let v5 = vec![10, 13, 15, 28];
+
+    // get by index
+    let third: &i32 = &v5[2];
+    println!("3rd: {}", third);
+
+    // get by index(when index out of range)
+    // this causes an error
+    // let nineth: &i32 = &v5[8];
+    // println!("9th: {}", nineth);
+
+    // get by get method
+    let third: Option<&i32> = v5.get(2);
+    println!("3rd: {:?}", third); // return Some(15)
+
+    // get by get method(when index out of range)
+    let tenth: Option<&i32> = v5.get(9);
+    println!("10th: {:?}", tenth); // return None
 }
