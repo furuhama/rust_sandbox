7d531d5859bcb363aab04601f26fe7117ee6d057
furuhama <furuhama@users.noreply.github.com>
2018-01-30 21:39:48 +0900

    Set keys & values as vector first, combine them

--- src/my_module/hash_map.rs
+++ src/my_module/hash_map.rs
@@ -8,4 +8,10 @@ pub fn hash_map() {
     scores.insert(String::from("nyan"), 10);
     scores.insert(String::from("piyo"), 50);
     println!("{:?}", scores);
+
+    // set keys and values as vector first, combine them
+    let teams = vec![String::from("nyan"), String::from("piyo")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
 }
