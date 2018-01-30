887c2db8e9470a8df5bc1106748516ce896f33cd
furuhama <furuhama@users.noreply.github.com>
2018-01-30 21:32:04 +0900

    HashMap Insertion

--- src/my_module/hash_map.rs
+++ src/my_module/hash_map.rs
@@ -1,5 +1,11 @@
 // Hash Map
 
+use std::collections::HashMap;
+
 pub fn hash_map() {
-    println!("{}", 102);
+    let mut scores = HashMap::new();
+    // basic insertion
+    scores.insert(String::from("nyan"), 10);
+    scores.insert(String::from("piyo"), 50);
+    println!("{:?}", scores);
 }
