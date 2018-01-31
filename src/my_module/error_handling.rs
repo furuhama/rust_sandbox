170dfe1c0d03da272d328cee6aeace60cb4b597f
furuhama <furuhama@users.noreply.github.com>
2018-01-31 12:45:42 +0900

    Add some ways of error handling

--- src/my_module/error_handling.rs
+++ src/my_module/error_handling.rs
@@ -1,9 +1,11 @@
 // panic!
+use std::io;
+use std::io::Read;
+use std::fs::File;
+use std::io::ErrorKind;
+
 
 pub fn error_handling() {
-    use std::fs::File;
-    use std::io::ErrorKind;
-
     // this returns a panic error
     // panic!("crash and burn!");
 
@@ -11,11 +13,80 @@ pub fn error_handling() {
     // let v = vec![1, 2, 3];
     // v[100];
 
+    // let f = File::open("hello.txt");
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => {
+    //         panic!("There was a problem opening the file: {:?}", error)
+    //     },
+    // };
+
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(ref error) if error.kind() == ErrorKind::NotFound => {
+    //         match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => {
+    //                 panic!(
+    //                     "Tried to create file but there was a problem: {:?}",
+    //                     e
+    //                 )
+    //             },
+    //         }
+    //     },
+    //     Err(error) => {
+    //         panic!(
+    //             "There was a problem opening the file: {:?}",
+    //             error
+    //         )
+    //     },
+    // };
+
+    // let f = File::open("hello.txt").unwrap();
+
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+
+    // these 3 ways are the same
+    let my_error = read_username_from_file();
+    println!("{:?}", my_error);
+
+    let my_error2 = read_username_from_file2();
+    println!("{:?}", my_error2);
+
+    let my_error3 = read_username_from_file3();
+    println!("{:?}", my_error3);
+
+    // this occurs an error
+    // `?` can be used only in function that returns a result
+    // let f = File::open("hello.txt")?;
+}
+
+// propagating error
+fn read_username_from_file() -> Result<String, io::Error> {
     let f = File::open("hello.txt");
-    let f = match f {
+    let mut f = match f {
         Ok(file) => file,
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        },
+        Err(e) => return Err(e),
     };
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    // `?` placed after Result value works in the same way as match control
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    // `?` placed after Result value works in the same way as match control
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+// shorten read_username_from_file2()
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
