aa70fd527a168806747142ce3bf5b0c888980200
furuhama <furuhama@users.noreply.github.com>
2018-01-31 10:44:07 +0900

    Index panic

--- src/my_module/error_handling.rs
+++ src/my_module/error_handling.rs
@@ -1,5 +1,10 @@
 // panic!
 
 pub fn error_handling() {
-    panic!("crash and burn!");
+    // this returns a panic error
+    // panic!("crash and burn!");
+
+    // this returns index out of bounds error
+    let v = vec![1, 2, 3];
+    v[100];
 }
