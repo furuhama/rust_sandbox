0f150ceed90d6038429310319e62456a16645ba7
furuhama <furuhama@users.noreply.github.com>
2018-01-31 11:24:23 +0900

    Show File open error

--- src/my_module/error_handling.rs
+++ src/my_module/error_handling.rs
@@ -1,10 +1,21 @@
 // panic!
 
 pub fn error_handling() {
+    use std::fs::File;
+    use std::io::ErrorKind;
+
     // this returns a panic error
     // panic!("crash and burn!");
 
     // this returns index out of bounds error
-    let v = vec![1, 2, 3];
-    v[100];
+    // let v = vec![1, 2, 3];
+    // v[100];
+
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
 }
