25f4b0ba28c1207268ca2fb9fde0b35440d5dfab
furuhama <furuhama@users.noreply.github.com>
2018-02-06 08:49:14 +0900

    Add functions for lifetime(does not work yet)

--- src/my_module/lifetime.rs
+++ src/my_module/lifetime.rs
@@ -1,5 +1,7 @@
 // lifetime
 
+use std::fmt::Display;
+
 pub fn lifetime() {
     // let r;
     // {
@@ -14,6 +16,13 @@ pub fn lifetime() {
     let string2 = "xyz";
     let result = longest(string1.as_str(), string2);
     println!("The longest string is: {}", result);
+
+    // let i = ImportantExcerpt { part: "nyaaaaaaaaaan" };
+    // let _ = i.announcement_and_return_part();
+
+    // let string3 = "xyz";
+    // let string4 = "xyzhogehoge";
+    // longest_with_an_announcement(string3, string4, i);
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -24,3 +33,27 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
