a0415947b34f72be046f9fc80124627cdcb97b4b
furuhama <furuhama@users.noreply.github.com>
2018-02-09 15:39:25 +0900

    Add struct & impl test

--- src/my_test/basic.rs
+++ src/my_test/basic.rs
@@ -19,5 +19,23 @@ mod tests {
     // fn another() {
     //     panic!("Make this test fail!");
     // }
-}
 
+    #[test]
+    fn larger_can_hold_smailler() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 2, width: 3 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    struct Rectangle {
+        length: u32,
+        width: u32,
+    }
+
+    impl Rectangle {
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.length > other.length && self.width > other.width
+        }
+    }
+}
