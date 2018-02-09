0ffe02d9bceea67c6b990b5ab2e0968f3c2ca594
furuhama <furuhama@users.noreply.github.com>
2018-02-09 15:27:53 +0900

    Move tests to basic module

---
+++ src/my_test/basic.rs
@@ -0,0 +1,23 @@
+// basic tests
+
+#[cfg(test)]
+mod tests {
+    use my_module;
+
+    #[test]
+    fn it_works() {
+        my_module::hello_rust();
+    }
+
+    #[test]
+    fn assertion_equal() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    // this test will fail
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail!");
+    // }
+}
+
--- src/my_test/mod.rs
+++ src/my_test/mod.rs
@@ -1,22 +1,4 @@
 // set modules in my_tests
 
-#[cfg(test)]
-mod tests {
-    use my_module;
+mod basic;
 
-    #[test]
-    fn it_works() {
-        my_module::hello_rust();
-    }
-
-    #[test]
-    fn assertion_equal() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    // this test will fail
-    // #[test]
-    // fn another() {
-    //     panic!("Make this test fail!");
-    // }
-}
