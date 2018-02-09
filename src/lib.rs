3ef5d626561f50514aee8734be4837f6fd37af04
furuhama <furuhama@users.noreply.github.com>
2018-02-09 15:24:23 +0900

    Move tests to modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,4 @@
 // set original modules in this file
 
 pub mod my_module;
-
-#[cfg(test)]
-mod tests {
-    use my_module;
-
-    #[test]
-    fn it_works() {
-        my_module::hello_rust();
-    }
-
-    #[test]
-    fn assertion_equal() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    // this test will fail
-    // #[test]
-    // fn another() {
-    //     panic!("Make this test fail!");
-    // }
-}
+pub mod my_test;
