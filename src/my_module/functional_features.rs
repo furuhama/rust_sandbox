941108b25969d673e7bf526fb89b8dfe5d30fd0c
furuhama <furuhama@users.noreply.github.com>
2018-02-13 23:15:28 +0900

    Use closure

--- src/my_module/functional_features.rs
+++ src/my_module/functional_features.rs
@@ -13,24 +13,21 @@ pub fn functional_features() {
     );
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result =
-        simulated_expensive_calculation(intensity);
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_result
+            expensive_closure(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_result
+            expensive_closure(intensity)
         );
     } else {
         if random_number == 3 {
@@ -38,7 +35,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result
+                expensive_closure(intensity)
             );
         }
     }
