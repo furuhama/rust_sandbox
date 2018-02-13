645d2862f0e7967e17cfd660aeb078a6767b12c2
furuhama <furuhama@users.noreply.github.com>
2018-02-13 21:22:00 +0900

    Use strerr instead of strout

--- src/my_module/minigrep.rs
+++ src/my_module/minigrep.rs
@@ -12,12 +12,13 @@ pub fn minigrep() {
 
     // args[0] == "target/debug/hello_world"
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
+
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
