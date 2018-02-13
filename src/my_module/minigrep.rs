06691bc277a00f16f3becce205cec869dec4778c
furuhama <furuhama@users.noreply.github.com>
2018-02-13 18:35:39 +0900

    Refactor to use Result & process::exit(1)

--- src/my_module/minigrep.rs
+++ src/my_module/minigrep.rs
@@ -3,13 +3,17 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 pub fn minigrep() {
     // just read arguments and return them
     let args: Vec<String> = env::args().collect();
 
     // args[0] == "target/debug/hello_world"
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.filename);
@@ -30,15 +34,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         // error handling, when the number of arguments is less than expected
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
