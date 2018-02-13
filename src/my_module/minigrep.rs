1dfe69f3a10ff1ddb3a05dbe009e3ce17cf12cf5
furuhama <furuhama@users.noreply.github.com>
2018-02-13 11:29:12 +0900

    Add content reading

--- src/my_module/minigrep.rs
+++ src/my_module/minigrep.rs
@@ -1,6 +1,8 @@
 // implement mini grep command
 
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 pub fn minigrep() {
     // just read arguments and return them
@@ -12,4 +14,13 @@ pub fn minigrep() {
 
     println!("Searching for: {}", query);
     println!("In file: {}", filename);
+
+    // try to open file which name is given as filename
+    let mut f = File::open(filename).expect("file not found");
+
+    // try to set content from read file into contents
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file.");
+
+    println!("With text:\n{}", contents);
 }
