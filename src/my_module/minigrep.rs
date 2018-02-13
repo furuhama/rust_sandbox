22da16e3068925d87177c2341b803085ebdbdde4
furuhama <furuhama@users.noreply.github.com>
2018-02-13 11:09:15 +0900

    Move args[0] to comment

--- src/my_module/minigrep.rs
+++ src/my_module/minigrep.rs
@@ -6,11 +6,10 @@ pub fn minigrep() {
     // just read arguments and return them
     let args: Vec<String> = env::args().collect();
 
-    let index_zero = &args[0];
+    // args[0] == "target/debug/hello_world"
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Zero index: {}", index_zero);
     println!("Searching for: {}", query);
     println!("In file: {}", filename);
 }
