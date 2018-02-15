82d279843d761c065f984a123d6e2e9a83164d42
furuhama <furuhama@users.noreply.github.com>
2018-02-15 18:58:00 +0900

    Use iterator in minigrep

--- src/my_module/minigrep.rs
+++ src/my_module/minigrep.rs
@@ -8,12 +8,20 @@ use std::error::Error;
 
 pub fn minigrep() {
     // just read arguments and return them
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
     // args[0] == "target/debug/hello_world"
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+    // let config = Config::new(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {}", err);
 
+    //     process::exit(1);
+    // });
+
+    // ======================================================
+
+    // just set env::args() as argument
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -25,17 +33,27 @@ pub fn minigrep() {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // old implementation
+
+    // let mut results = Vec::new();
 
     // iterate contents for each line,
     // if it contains query, its line will be pushed to results vec.
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    // ================================================================
+
+    // use iterator
+
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -80,14 +98,26 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
+    fn new(mut args: env::Args) -> Result<Config, &'static str> {
         // error handling, when the number of arguments is less than expected
         if args.len() < 3 {
             return Err("not enough arguments");
         }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        // not to use clone() method
+        // let query = args[1].clone();
+        // let filename = args[2].clone();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         // read env vars
         // CASE_INSENSITIVE=1 cargo run [query] [contents]
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
