1c21522fc9ae4091b0c7255341d44dc1796c7027
furuhama <furuhama@users.noreply.github.com>
2018-02-15 16:35:55 +0900

    Add move using test & It exactly failed

--- src/my_test/functional_features.rs
+++ src/my_test/functional_features.rs
@@ -26,4 +26,17 @@ mod test {
 
         assert!(equal_to_x(y));
     }
+
+    #[test]
+    fn use_move() {
+        let x = vec![1, 2, 3];
+
+        let equal_to_x = move |z| z == x;
+
+        println!("can't use x here: {:?}", x);
+
+        let y = vec![1, 2, 3];
+
+        assert!(equal_to_x(y));
+    }
 }
