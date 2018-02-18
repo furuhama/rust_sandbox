379aabd691ba706f3a8f8e577ef6b2a6e35d8d9e
furuhama <furuhama@users.noreply.github.com>
2018-02-18 16:48:20 +0900

    Add docmentation for hello_world crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! # hello_world Crate
+//!
+//! `hello_world` is a collection of `The Rust Programming Language - second edition`'s scripts.
+
+/// main module is my_module
+/// main test module is my_test
+
 // set original modules in this file
 
 pub mod my_module;
