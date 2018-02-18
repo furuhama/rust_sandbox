e6ae208fe0a0421cfd9c592f31e3c8b13ebde7b8
furuhama <furuhama@users.noreply.github.com>
2018-02-18 16:50:32 +0900

    Fix docmentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 //!
 //! `hello_world` is a collection of `The Rust Programming Language - second edition`'s scripts.
 
-/// main module is my_module
-/// main test module is my_test
-
 // set original modules in this file
 
+/// main module is `my_module`
+
 pub mod my_module;
+
+/// main test module is `my_test`
+
 pub mod my_test;
