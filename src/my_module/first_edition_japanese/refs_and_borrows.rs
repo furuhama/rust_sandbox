fe3c376c8c1e0b2f47967d0cef67b60ed942d47d
furuhama <furuhama@users.noreply.github.com>
2018-03-21 21:04:08 +0900

    Define references_and_borrowings & Add comments

--- src/my_module/first_edition_japanese/refs_and_borrows.rs
+++ src/my_module/first_edition_japanese/refs_and_borrows.rs
@@ -1,5 +1,20 @@
 // References and Borrowings
 
 pub fn references_and_borrowings() {
-    println!("hoge");
+    // define x as mutable (default: i32) value,
+    // to edit x later in child scope
+    let mut x = 5;
+    // make new child scope
+    {
+        // make &mut reference only in child scope
+        let y = &mut x;
+        // to use referenced value(= `x`),
+        // add * mark at the head of `y`
+        *y += 1;
+        // at the end of this scope,
+        // the reference of `x` by `y` ends
+    }
+    // `println!` macro references argument as an immutable variable
+    // (= & reference, not &mut reference)
+    println!("use &mut reference: {}", x);
 }
