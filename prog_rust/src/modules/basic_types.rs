a6b08562ef6221c17fd005a7b4c76fb1d9cf74da
furuhama <furuhama@users.noreply.github.com>
2018-04-18 10:06:29 +0900

    Add closure fn

--- prog_rust/src/modules/basic_types.rs
+++ prog_rust/src/modules/basic_types.rs
@@ -3,6 +3,7 @@
 pub fn basic_types() {
     println!("{:?}", build_vec_verbose());
     println!("{:?}", build_vec());
+    closure();
 }
 
 fn build_vec_verbose() -> Vec<i16> {
@@ -17,4 +18,11 @@ fn build_vec() -> Vec<i16> {
     v.push(10);
     v.push(20);
     v
+}
+
+fn closure() {
+    let is_even = |x: u64| -> bool { x % 2 == 0 };
+    let d: u64 = 16;
+
+    println!("{} is even?: {}", d, is_even(d));
 }
