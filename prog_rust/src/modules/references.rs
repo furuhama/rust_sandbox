4c33431155132b333a7499af78528b20faa3e16a
furuhama <furuhama@users.noreply.github.com>
2018-05-29 00:24:16 +0900

    Add references

--- prog_rust/src/modules/references.rs
+++ prog_rust/src/modules/references.rs
@@ -8,4 +8,12 @@ pub fn references() {
         r = &y;
     }
     assert!(*r == 20);
+
+    #[allow(dead_code)]
+    struct Point { x: i32, y: i32 }
+    let point = Point { x: 1000, y: 3792 };
+    let r = &point;
+    let rr = &r;
+    let rrr = &rr;
+    assert_eq!(rrr.y, 3792);
 }
