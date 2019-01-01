1aaab6d0fa0d2a94e0cced96a87a50f4710f4cd2
furuhama <furuhama@users.noreply.github.com>
2019-01-01 11:06:53 +0900

    Quit setting left value when pattern matching with () Unit return value

--- src/my_module/pattern_match.rs
+++ src/my_module/pattern_match.rs
@@ -14,7 +14,7 @@ pub fn pattern_matching() {
 
 fn conditional_arm() {
     let target = Some(10);
-    let _ = match target {
+    match target {
         Some(ref n) if n%10 == 0 => {
             println!("the number can be divided by 10!: {}", n);
         },
