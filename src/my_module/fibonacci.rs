50088e8a6a037b50dbc89cfc99706975f9ad4f47
furuhama <furuhama@users.noreply.github.com>
2019-01-01 11:28:02 +0900

    Quit passing reference when function arguments have native number types like i32

--- src/my_module/fibonacci.rs
+++ src/my_module/fibonacci.rs
@@ -3,16 +3,16 @@
 pub fn fibonacci() {
     let n: i32 = 90;
 
-    println!("{}th fibonacci number: {}", n, get_fibonacci(&n));
-    println!("{}th fibonacci number: {}", n, get_fibonacci_tuple(&n));
+    println!("{}th fibonacci number: {}", n, get_fibonacci(n));
+    println!("{}th fibonacci number: {}", n, get_fibonacci_tuple(n));
 }
 
-fn get_fibonacci(n: &i32) -> u64 {
+fn get_fibonacci(n: i32) -> u64 {
     let mut a: u64 = 1;
     let mut b: u64 = 0;
     let mut counter: i32 = 1;
 
-    if *n == 1 {
+    if n == 1 {
         return 1;
     }
 
@@ -22,18 +22,18 @@ fn get_fibonacci(n: &i32) -> u64 {
         b = a;
         a = tmp;
 
-        if counter == *n {
+        if counter == n {
             break;
         }
     }
     a
 }
 
-fn get_fibonacci_tuple(n: &i32) -> i64 {
+fn get_fibonacci_tuple(n: i32) -> i64 {
     let mut tup: (i64, i64) = (1, 0);
     let mut counter: i32 = 1;
 
-    if *n == 1 {
+    if n == 1 {
         return 1;
     }
 
@@ -41,7 +41,7 @@ fn get_fibonacci_tuple(n: &i32) -> i64 {
         counter += 1;
         tup = (tup.0 + tup.1, tup.0);
 
-        if counter == *n {
+        if counter == n {
             break;
         }
     }
